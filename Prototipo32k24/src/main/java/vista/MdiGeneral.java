8163d0941cabccff6a0f40bde8a1f7b2722f96bc
Jose García <joseagmont@gmail.com>
2024-05-03 17:21:08 -0600

    Segundo proceso

--- Prototipo32k24/src/main/java/vista/MdiGeneral.java
+++ Prototipo32k24/src/main/java/vista/MdiGeneral.java
@@ -102,7 +102,7 @@ public class MdiGeneral extends javax.swing.JFrame {
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
-        MantenimientoVendedores ventana = new MantenimientoVendedores();
+        MantenimientoAlumnos ventana = new MantenimientoAlumnos();
         jDesktopPane1.add(ventana);
         Dimension desktopSize = jDesktopPane1.getSize();
         Dimension FrameSize = ventana.getSize();

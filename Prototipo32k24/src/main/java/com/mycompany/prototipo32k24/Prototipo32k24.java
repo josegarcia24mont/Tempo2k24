80ff5c91ca9cbba6a79e75a835844056deefa6f6
Jose García <joseagmont@gmail.com>
2024-05-03 17:10:22 -0600

    Primer proceso

---
+++ Prototipo32k24/src/main/java/com/mycompany/prototipo32k24/Prototipo32k24.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prototipo32k24;
+
+/**
+ *
+ * @author Joseg
+ */
+public class Prototipo32k24 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
